024706023f14cc306ce30796bc1aa1f3393775d9
Pasindu Thiwanka <pasindu.17@itfac.mrt.ac.lk>
2020-05-22 13:10:21 +0530

    Assignmnet 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import {ServerComponent} from "./Servers/server.component";
+import { SubServerComponent } from './sub-server/sub-server.component';
+import { RedComponent } from './red/red.component';
+import { GreenComponent } from './green/green.component';
+import { YellowComponent } from './yellow/yellow.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    SubServerComponent,
+    RedComponent,
+    GreenComponent,
+    YellowComponent
   ],
   imports: [
-    BrowserModule,
     ReactiveFormsModule,
+    BrowserModule,
     FormsModule
   ],
   providers: [],
