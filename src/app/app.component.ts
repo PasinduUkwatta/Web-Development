f04926dc7358509e3799608670d3bb9d645cd005
Pasindu Thiwanka <pasindu.17@itfac.mrt.ac.lk>
2020-05-23 08:28:08 +0530

    workspace

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,34 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  ServerName ='Applo';
-  ServerStatues='offline';
-  ServerId =11;
-  statuesFlag = false;
-  buttonState = true;
 
-
-  //this is the constructor for putting the button in active for 5 secs.
-  title: Function;
-  constructor() {
-    setTimeout(()=> {
-      this.buttonState = false;
-
-    },5000)
-
-
-  }
-
-  toggleServerStatues(){
-    this.statuesFlag=!this.statuesFlag;
-    if(this.statuesFlag ===true){
-
-      this.ServerStatues ='online';
-    }
-    else {
-      this.ServerStatues = 'offline';
-    }
-
-    return this.ServerStatues;
-  }
 }
