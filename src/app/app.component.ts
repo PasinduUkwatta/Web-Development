a4dcf5bf0fca2dfe289e14600ca253d2a4178b85
Pasindu Thiwanka <pasindu.17@itfac.mrt.ac.lk>
2020-05-23 05:53:46 +0530

    Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'Pasindu';
+  ServerName ='Applo';
+  ServerStatues='offline';
+  ServerId =11;
+  statuesFlag = false;
+  buttonState = true;
+
+
+  //this is the constructor for putting the button in active for 5 secs.
   title: Function;
+  constructor() {
+    setTimeout(()=> {
+      this.buttonState = false;
+
+    },5000)
+
+
+  }
+
+  toggleServerStatues(){
+    this.statuesFlag=!this.statuesFlag;
+    if(this.statuesFlag ===true){
+
+      this.ServerStatues ='online';
+    }
+    else {
+      this.ServerStatues = 'offline';
+    }
+
+    return this.ServerStatues;
+  }
 }
