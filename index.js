1bee2e6d55ff80a41f6f684119f71a61b06c5816
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-06-30 15:22:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+
+//Import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom"
+
+
+//Craete a react Component
+const App = function () {
+    return (<div>Hi! There</div>)
+};
+
+
+//Take the react Component and show it on the screeen
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+)
+
+
+
+
+
+
