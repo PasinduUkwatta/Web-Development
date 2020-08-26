d9c5dfedb49714cfd51bd5a2b4a521033886e71e
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-08-27 00:10:16 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-import {combineReducers} from 'redux';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const songsReducer =()=>{
-   return [
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-       {title: 'No Scrubs', duration: '4:05'},
-       {title: 'Marcrena', duration: '3:45'},
-       {title: 'All Star', duration: '3:27'},
-       {title: 'I Want In that Way', duration: '5:16'}
-
-   ]
-}
-
-const selectedSongReducer=(selectedSong=null,action)=>{
-    if (action.type==="SONG_SELECTED"){
-        return action.payload
-    }
-
-    return selectedSong
-
-}
-
-export default combineReducers({
-    songs: songsReducer,
-    selectedSong: selectedSongReducer
-})
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
