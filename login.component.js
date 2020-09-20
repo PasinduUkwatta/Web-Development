47760d56890ae4fb738e7e5314dc99ef741ef8e3
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-09-20 22:30:47 +0530

    Add files via upload

--- login.component.js
+++ login.component.js
@@ -1,59 +1,147 @@
-import React, { Component ,useEffect} from "react";
+import React, { Component } from "react";
+import axios from "axios"
+import Profile from './profile.component'
+import {connect} from "react-redux"
 
-export default class Login extends Component {
+
+class Login extends Component {
+   
 
     constructor(props) {
         super (props)
 
         this.state ={
             email: '',
-            password: ''
+            password: '',
+            data:[]
         }
+        this.changeHandler = this.changeHandler.bind(this);
+        this.submitHandler = this.submitHandler.bind(this);
+        
+       
     }
 
-    handleEmailChange=(event)=>{
-        this.setState({
-            email: event.target.value
+    changeHandler = e => {
+        this.setState({[e.target.name]: e.target.value})
+    }
 
+
+       submitHandler = e => {
+        e.preventDefault()
+       
+        console.log(this.state);
+        axios.post('/sign-in',this.state,{
+            headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        } 
         })
-    }
+            .then(response => {
+                console.log(response)
+                this.setState({data:response.data});
+                console.log(this.state)
+               
+                
+               
+                
+                if(response.data==[]){
+                    this.props.history.push('/payment')
+                    console.log("User did not have any payment yet")
+                }
 
-    handlePasswordChange=(event)=>{
-        this.setState({
-            password: event.target.value
+                else if(response.data =="user details are invalid"){
+                    this.props.history.push('/sign-up')
+                    console.log("Please Try Again")
+                                       
+                }
+                
+                else{
+                    
+                    console.log("Welcome to the site")
+                    this.props.history.push({
+                        pathname: '/profile',
+                        state: { email: this.state.email,
+                                password: this.state.password }
+                               
+                            }) 
 
-        })
-    }
+                }
+               
 
-    handleSubmit =(event)=>{
-        alert(`
-                Email :${this.state.email}
-                Password : ${this.state.password}`
+            })
+            .catch(error => {
+                console.log(error.response)
+               
+                console.log("Please Try Again")
+                this.props.history.push('/sign-in')
+            }
+            
         )
-        event.preventDefault()
-        this.props.history.push('/sign-up')
 
-    }
+        }
+  
 
     render() {
+        const{email, password} = this.state
+     
+
         return (
-            <form onSubmit={this.handleSubmit}>
+
+            <form onSubmit={this.submitHandler}  >
+            
                 <h3>Sign In</h3>
+
+                
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" value={this.state.email} onChange={this.handleEmailChange}  />
+                    <input 
+                    type="email" 
+                    name ="email"
+                    className="form-control" 
+                    placeholder="Enter email" 
+                    value={email} 
+                    onChange={this.changeHandler}  />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" value={this.state.password} onChange={this.handlePasswordChange}  />
+                    <input 
+                    type="password" 
+                    name="password"
+                    className="form-control" 
+                    placeholder="Enter password" 
+                    value={password} 
+                    onChange={this.changeHandler}  />
                 </div>
 
                 
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                <button 
+                onClick={(e)=>this.props.setName(e)}
+                type="submit" 
+                className="btn btn-primary btn-block">
+                Sign In
+                </button>
                 
             </form>
         );
     }
 }
+
+const mapStateToProps = (state) =>{
+    return { 
+        email: state.email,
+        password: state.password
+
+     }
+}
+
+const mapDispatchToProps = (dispatch)=>{
+    return{
+        setName(){
+            dispatch({type:"SET_NAME"})
+        }
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Login)
