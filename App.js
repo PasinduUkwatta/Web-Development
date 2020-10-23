6d4672873934b03df607c0975ffe893bba842354
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-10-23 22:02:50 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Button,Form, Input,FormGroup,Label} from 'reactstrap';
- function App() {
-  return (
-    <Form className="login-form">
-      <h1>
-          <span className ="font-weight-bold">User Login</span>
-      </h1>
-        <h2 className="text-center">
-            Welcome !
-        </h2>
-        <FormGroup>
-            <label>E-mail</label>
-            <input type="email" placeholder ="Email"/>
-        </FormGroup>
 
-        <FormGroup>
-            <label>Password</label>
-            <input type="password" placeholder ="Password"/>
-        </FormGroup>
-        <Button className ="btn-lg btn-dark btn-block">Log in</Button>
-    </Form>
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
