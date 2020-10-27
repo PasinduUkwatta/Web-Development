b2e0676a0f9ad1c227330242799c51881ef1cd8e
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-10-27 12:07:34 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-//this is a simple variable create in node js 
-//to run this in cmd , we can  type the node and the specific file name 
+var budjetController =(function (){
+   var x=23
+    var add =function(a){
+       return x+a
+    }
 
-var firstName ="pasindu";
-
-console.log(firstName);
-
-//when we use == js only consider about the value of the data
-//when we use === js consider about the data type and the value
-var age1 = 30;
-var age2 ="30";
-var result1 = age1==age2;
-var result2 = age1===age2;
-
-console.log(result1);
-console.log(result2);
+    return{
+       publicTest :function(b){
+          console.log(add(b))
+       }
+    }
+})()
