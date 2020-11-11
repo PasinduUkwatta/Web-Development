e36ba5320d42b9ac013667ada1ab71c54bae64db
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-11-11 09:56:57 +0530

    Add files via upload

--- components/App.js
+++ components/App.js
@@ -1,23 +1,32 @@
-import React from "react"
-import SongList from "./SongList";
-import SongDetail from "./SongDetail";
+import React from "react";
+import SearchBar from "./SearchBar";
+import unsplash from "../api/unsplash";
+import ImageList from "./ImageList";
 
-const App =()=>{
-    return(
-        <div className="ui container grid">
-            <div className="ui row">
-                <div className="column eight wide">
-                    <SongList/>
+class App extends React.Component{
+    state ={
+        images :[]
+    }
 
-                </div>
-                <div className="column eight wide">
-                    <SongDetail/>
-                </div>
+  onSearchSubmit= async (term)=>{
+      const responce = await unsplash.get('/search/photos',{
+            params :{query:term},
 
+        })
+
+       this.setState({images:responce.data.results})
+    }
+
+    render() {
+        return (
+            <div className="ui container" style={{marginTop:"15px"}}>
+                <SearchBar onSubmit={this.onSearchSubmit}/>
+                <ImageList images={this.state.images}/>
             </div>
+        )
+    }
 
-        </div>
-    )
+
 }
 
 export default App
