720b2ed70f30c74032a979d6259e315f7590f6d5
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-11-19 07:38:19 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {SIGN_IN,SIGN_OUT} from "./types";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export const signIn =(userId) =>{
+    return{
+        type:SIGN_IN,
+        payload :userId
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export const signOut =() =>{
+    return{
+        type:SIGN_OUT
+    }
+}
